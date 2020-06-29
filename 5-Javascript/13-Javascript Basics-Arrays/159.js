044b8c099e09e4c2c4c6ee2a805224bef6abab25
Sciarbel <sciarbel@gmail.com>
2020-06-28 23:49:44 -0300

    Added class 160

--- 5-Javascript/13-Javascript Basics-Arrays/159.js
+++ 5-Javascript/13-Javascript Basics-Arrays/159.js
@@ -1,18 +1,13 @@
-var todos = ["Buy aligator"];
-
-var input = prompt("What would you like to do?")
-
-while (input !== "quit") {
-  // handle input
-  if (input === "list") {
-    console.log(todos);
-  } else if (input === "new") {
-    // ask for a new todo
-    var newTodo = prompt("Enter new todo");
-    // add to todos array
-    todos.push(newTodo);
-  }
-  // ask again for new input
+  var todos = [];
   input = prompt("What would you like to do?");
-}
-console.log("Ok, quit the app.");
+  while (input !== "quit") {
+    if (input === "list") {
+      console.log(todos);
+    } else if (input === "new") {
+      var newtodo = prompt("Add a todo");
+      todos.push(newtodo);
+    }
+    input = prompt("What would you like to do?");
+  }
+  console.log("OK, exit list");
+
---
+++ 5-Javascript/13-Javascript Basics-Arrays/160.js
@@ -0,0 +1,19 @@
+var colors = ["red", "orange", "yellow", "green"];
+
+// Ex-1
+// for (var i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// }
+
+// Ex-2
+// colors.forEach(function (color) {
+//   console.log(colors);
+// });
+
+// Ex-3
+// "test" is just a placeholder for the function
+colors.forEach(function (test) {
+  console.log(test);
+});
+
+
