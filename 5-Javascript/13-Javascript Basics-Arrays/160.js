42eaf40f594a9e639f89f709a7a23e3af57aed66
Sciarbel <sciarbel@gmail.com>
2020-06-30 23:53:51 -0300

    Finished class 160

--- 5-Javascript/13-Javascript Basics-Arrays/160.js
+++ 5-Javascript/13-Javascript Basics-Arrays/160.js
@@ -5,15 +5,30 @@ var colors = ["red", "orange", "yellow", "green"];
 //   console.log(colors[i]);
 // }
 
+
 // Ex-2
 // colors.forEach(function (color) {
 //   console.log(colors);
 // });
 
+
 // Ex-3
 // "test" is just a placeholder for the function
-colors.forEach(function (test) {
-  console.log(test);
-});
+// colors.forEach(function (test) {
+//   console.log(test);
+// });
 
 
+// Ex-4
+// function printColor(color) {
+//   console.log("Your color is " + color);
+// }
+
+// colors.forEach(printColor);
+
+// Ex-5 - While loop
+var count = 0;
+while (count < colors.length) {
+  console.log(colors[count]);
+  count++;
+}
