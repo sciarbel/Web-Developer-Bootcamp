0915b56e82cdcded5cbfeb8d7e855a0a92b95669
Sciarbel <sciarbel@gmail.com>
2020-07-16 00:14:06 -0300

    Finished Class 133

--- 5-Javascript/11-Javascript Basics-Control Flow/133.js
+++ 5-Javascript/11-Javascript Basics-Control Flow/133.js
@@ -1,7 +1,25 @@
 // While loops
+
+// Ex 1
 var count = 1;
 
 while (count < 6) {
   console.log("Count is " + count);
   count++;
+}
+
+
+// Ex 2
+while (count <= 20) {
+  console.log("Count is " + count);
+  count += 2;
+}
+
+// Ex 3
+var str = "hello";
+
+var num = 0;
+while (num < str.length) {
+  console.log(str[num]);
+  num++;
 }
