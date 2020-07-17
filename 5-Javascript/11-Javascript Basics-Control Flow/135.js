b018c282a7c7ad46508012c801541afd7830987f
Sciarbel <sciarbel@gmail.com>
2020-07-17 19:38:45 -0300

    Remade Class 135 exercises

--- 5-Javascript/11-Javascript Basics-Control Flow/135.js
+++ 5-Javascript/11-Javascript Basics-Control Flow/135.js
@@ -1,37 +1,33 @@
-console.log("ex 1");
-
+// Ex 1
 var num = -10;
 while (num <= 19) {
   console.log(num);
   num++;
 }
 
-console.log("ex 2");
-
-var num = 10;
-while (num <= 40) {
-  if (num % 2 === 0) {
-    console.log(num);
+// Ex 2
+var numEven = 10;
+while (numEven <= 40) {
+  if (numEven % 2 === 0) {
+    console.log(numEven);
   }
-  num++;
+  numEven++;
 }
 
-console.log("ex 3");
-
-var num = 300;
-while (num <= 333) {
-  if (num % 2 !== 0) {
-    console.log(num);
+// Ex 3
+var numOdd = 300;
+while (numOdd <= 333) {
+  if (numOdd % 2 !== 0) {
+    console.log(numOdd);
   }
-  num++;
+  numOdd++;
 }
 
-console.log("ex 4");
-
-var num = 5;
-while (num <= 50) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    console.log(num);
+// Ex 4
+var numDiv = 5;
+while (numDiv <= 50) {
+  if (numDiv % 3 === 0 && numDiv % 5 === 0) {
+    console.log(numDiv);
   }
-  num++;
+  numDiv++;
 }
