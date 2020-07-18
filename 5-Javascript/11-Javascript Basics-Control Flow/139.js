a9492443a7c6ea99a441f181e816154c0b0ceb96
Sciarbel <sciarbel@gmail.com>
2020-07-17 23:06:02 -0300

    Added Class 139

---
+++ 5-Javascript/11-Javascript Basics-Control Flow/139.js
@@ -0,0 +1,21 @@
+// for (var count = 0; count < 6; count++) {
+//   console.log(count);
+// }
+
+// var count = 0;
+// while (count < 6) {
+//   console.log(count);
+//   count++;
+// }
+
+var word = "hello";
+for (var count = 0; count < word.length; count++) {
+  console.log(word[count]);
+}
+
+var word = "hello";
+var count = 0;
+while (count < word.length) {
+  console.log(word[count]);
+  count++;
+}
