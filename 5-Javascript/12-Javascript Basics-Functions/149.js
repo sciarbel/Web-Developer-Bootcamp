de6570561073f21a73f6726a448ab78b01e0c94a
Sciarbel <sciarbel@gmail.com>
2020-07-20 15:17:21 -0300

    Remade Class 149 Exercises

--- 5-Javascript/12-Javascript Basics-Functions/149.js
+++ 5-Javascript/12-Javascript Basics-Functions/149.js
@@ -1,36 +1,37 @@
-// Ex 1 - isEven
-
-// v1
+// Ex 1
 // function isEven(num) {
 //   if (num % 2 === 0) {
 //     return true;
-//   } else {
-//     return false;
 //   }
+//   return false;
 // }
 
-// v2
-// function isEven(num) {
-//   return num % 2 === 0;
-// }
+function isEven(num) {
+  return num % 2 === 0;
+}
 
-
-// EX 2 - factorial
+// Ex 2
+function factorial(num) {
+  var result = 1;
+  for (var i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+} 
 
 // function factorial(num) {
-//   var result = 1;
-//   for (var i = 1; i <= num; i++) {
-//     result = result *= i; //result *1;
+//   if (num === 0) {
+//     return 1;
+//   }
+//   var result = num;
+//   for (var i = num - 1; i >= 1; i--) {
+//     result *= i;
 //   }
 //   return result;
 // }
 
 
-// Ex 3 kebabToSnake
-
+// Ex 3
 function kebabToSnake(str) {
-  var newStr = str.replace(/-/g, "_");
-  return newStr;
+  return str.replace(/-/g, "_");
 }
-kebabToSnake(str);
-// g flag in regular expressions is a global search, meaning it will match all occurrences
