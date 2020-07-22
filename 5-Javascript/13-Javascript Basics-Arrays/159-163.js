450678f7a529900be1f7f49674553db8e658c866
Sciarbel <sciarbel@gmail.com>
2020-07-22 20:33:13 -0300

    Remade |Class 159

--- 5-Javascript/13-Javascript Basics-Arrays/159-163.js
+++ 5-Javascript/13-Javascript Basics-Arrays/159-163.js
@@ -1,36 +1,15 @@
-// Class 159  
+// Class 159
 var todos = [];
 
-var input = prompt("What would you like to do?");
+var input = prompt("What yould you like to do?");
+
 while (input !== "quit") {
   if (input === "list") {
-    listTodos();
+    console.log(todos);
   } else if (input === "new") {
-    newTodo();
-  } else if (input === "delete") {
-    deleteTodo();
-  }
+    var newTodo = prompt("Enter new todo:");
+    todos.push(newTodo);
+  } 
   input = prompt("What would you like to do?");
 }
-console.log("OK, exit list");
-
-
-function listTodos() {
-  // todo, index: placeholders. The first gives the array item, and the second gives the index.
-  todos.forEach(function (todo, index) {
-    console.log(index + ": " + todo);
-  });
-}
-
-function newTodo() {
-  var newtodo = prompt("Add a todo");
-  todos.push(newtodo);
-  console.log("Added Todo");
-}
-
-function deleteTodo() {
-  var index = prompt("Which action do you want to delete? Type index");
-  // splice: 1st placeholder gives the position, and the second, how many itens after.
-  todos.splice(index, 1);
-  console.log("Todo Deleted");
-}
+console.log("Ok, quit app");
