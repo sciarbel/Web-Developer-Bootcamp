15ab6b5e8e05c8b1fcfd164899d323d810f47564
Charbel Shoiti Salvador Uchida <sciarbel@gmail.com>
2020-07-22 14:35:29 -0300

    Added Class 155

---
+++ 5-Javascript/13-Javascript Basics-Arrays/155.js
@@ -0,0 +1,12 @@
+var colors = ["red", "orange", "yellow"];
+
+colors[1];
+
+colors[3] = "blue";
+
+
+var friends = new Array(); /* uncommon */
+
+var nums = [45, 37, 89, 24];
+console.log(nums.length);
+
