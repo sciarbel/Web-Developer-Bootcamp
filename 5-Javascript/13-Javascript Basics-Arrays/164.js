51135f6cced2cef56eb108892aff9037217fb51f
Sciarbel <sciarbel@gmail.com>
2020-07-25 23:57:10 -0300

    Start Class 164 exercises

---
+++ 5-Javascript/13-Javascript Basics-Arrays/164.js
@@ -0,0 +1,22 @@
+// Ex 1 - printReverse()
+function printReverse([1, 2, 3, 4]) {
+  
+}
+
+
+// Ex 2 - isUniform()
+function isUniform([]) {
+  
+}
+
+
+// Ex 3 - sumArray()
+function sumArray() {
+
+}
+
+
+// Ex 4 - max()
+function max() {
+  ;
+}
