604e04b233fe35b09f95fd7bb3580a257d1350d3
Sciarbel <sciarbel@gmail.com>
2020-07-26 19:53:54 -0300

    Finished CLass 164

--- 5-Javascript/13-Javascript Basics-Arrays/164.js
+++ 5-Javascript/13-Javascript Basics-Arrays/164.js
@@ -1,22 +1,59 @@
-// Ex 1 - printReverse()
-function printReverse([1, 2, 3, 4]) {
-  
+// Ex 1
+// function printReverse(arr) {
+//   arr.reverse();
+//   arr.forEach(function(item) {
+//     console.log(item);
+//   });
+// }
+
+function printReverse(arr) {
+  for (i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
 }
 
 
-// Ex 2 - isUniform()
-function isUniform([]) {
-  
+// Ex 2
+function isUniform(arr) {
+  var frst = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] !== frst) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+// function isUniform(arr) {
+//   var first = arr[0];
+//   arr.forEach(function(item) {
+//     if (item !== arr[0]) {
+//       console.log(false);
+//     } else {
+//       console.log(true);
+//     }
+//   });
+// }
+
+
+// Ex 3
+function sumArray(arr) {
+  var total = 0;
+  arr.forEach(function (item) {
+    total += item;
+  });
+  return total;
 }
 
 
-// Ex 3 - sumArray()
-function sumArray() {
-
-}
-
-
-// Ex 4 - max()
-function max() {
-  ;
+// Ex 4
+function max(arr) {
+  var max = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  console.log(max);
 }
