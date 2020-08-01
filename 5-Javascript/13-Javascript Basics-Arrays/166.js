4ce576f7c54b5fbc65a1c85c54604e45a49dd454
Sciarbel <sciarbel@gmail.com>
2020-07-31 22:49:29 -0300

    Updated Class 166

--- 5-Javascript/13-Javascript Basics-Arrays/166.js
+++ 5-Javascript/13-Javascript Basics-Arrays/166.js
@@ -3,3 +3,21 @@ var nums = [45, 64, 77, 34];
 nums.forEach(function (num) {
   console.log(num);
 });
+
+
+// Ex 2
+// myForEach(nums, function (num) {
+//   console.log(num);
+// });
+
+
+// Ex 3
+function myForEach(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+}
+
+var colors = ["red", "orange", "yellow"];
+
+myForEach(colors, console.log);
